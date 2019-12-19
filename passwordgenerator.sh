fa721392c83b0a8aa66d386ce95c6b14bd37ab86
tanzilamohita <tanzilamohita@gmail.com>
2019-12-19 19:21:47 +0900

    Basic Shell Programming

---
+++ passwordgenerator.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# this can be anything of your choice
+echo "Welcome to simple password generator"
+
+# ask the user how much long should be
+echo "please enter the length of the password"
+
+# read the input given by user and store in variable
+read  PASS_LENGTH
+
+# loop will create 5 passwords according to
+# user as per length given by user
+for p in $(seq 1 5);
+do
+    openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+done
+
+# use for reducing immediately stop shell screen
+read test
--- sh_basic.sh
+++ sh_basic.sh
@@ -22,14 +22,14 @@ echo Value $val
 # taking input from user
 #===================================
 
-echo "Enter names: "
-read name1 name2 name3
+#echo "Enter names: "
+#read name1 name2
 
 #===================================
 # print newlines
 #===================================
 
-echo -e "1: $name1 \n2: $name2 \n3: $name3"
+#echo -e "1: $name1 \n2: $name2 \n" # -e = option enables the interpretation of backslash escapes
 NAME="Zara Ali"
 unset NAME
 echo $NAME
@@ -68,6 +68,57 @@ echo "First Method: ${NAME[*]}"
 echo "Second Method: ${NAME[@]}"
 
 #===================================
+# array Basic using for loop
+#===================================
+
+# To declare static Array
+arr=(1 2 3 4 5)
+
+# loops iterate through a
+# set of values until the
+# list (arr) is exhausted
+for i in "${arr[@]}"
+do
+    # access each element
+    # as $i
+    echo $i
+done
+
+#===================================
+# Read the array elements at run time and then Print the Array
+#===================================
+# To input array at run
+# time by using for-loop
+
+echo -n "Enter the Total numbers :"
+read n
+echo "Enter numbers:"
+i=0
+
+# Read upto the size of
+# given array starting
+# from index, i=0
+while [ $i -lt $n ]
+do
+    # To input from user
+    read a[$i]
+
+    # To increment index
+    # by 1, i=i+1
+    i=`expr $i + 1`
+done
+
+# Print the array starting
+# from index, i=0
+echo "Output :"
+
+for i in "${a[@]}"
+do
+    # access each element as $i
+    echo $i
+done
+
+#===================================
 # basic operators
 #===================================
 
@@ -90,10 +141,31 @@ mod=`expr $b % $a` # modulus
 echo Modulus: $mod
 
 #===================================
-# nested loop
+# for loop to print 1 to 10
+#===================================
+for((i=1; i<=10; i++))
+do
+  echo -n "$i "
+echo
+done
+
+#===================================
+# nested loop inverted half pyramid
 #===================================
 rows=5
-for((i=1; i<=rows; i++))
+for((i=rows; i>=1; i--))
+do
+  for((j=1; j<=i; j++))
+  do
+    echo -n "* "
+  done
+  echo
+done
+#===================================
+# nested loop half pyramid
+#===================================
+read n
+for((i=1; i<=n; i++))
 do
   for((j=1; j<=i; j++))
   do
@@ -102,8 +174,48 @@ do
   echo
 done
 
+#===================================
+# nested loop full pyramid
+#===================================
+read n
+for((i=1; i<=n; i++))
+do
+  for((j=1; j<=n-i; j++))
+  do
+    echo -n "  "
+  done
+  for((j=1; j<=2*i - 1; j++))
+  do
+    echo -n "* "
+  done
+  echo
+done
+
+#===================================
+# Decision making
+# prime number check
+#===================================
+read num
+f=0 # flag
+for((i=2; i<=num/2; i++))
+do
+  f=1
+  break
+done
+if [ $num == 1 ]
+then
+   echo "1 is neither prime nor composite."
+elif [ $f == 0 ]
+  then
+    echo "$num is a prime number."
+else
+    echo "$num is not a prime number."
+fi
+
 # use for reducing immediately stop shell screen
 read test
+
+
 
 
 
